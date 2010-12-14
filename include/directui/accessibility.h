9b61e32569597f8f4dba0796b82e727931fa5fe2
James <james.keane@gmail.com>
2010-12-14 14:38:28 -0500

    More testing, added support for both x86 and x64 compiling.  As well as fixed some definitions.

--- include/directui/accessibility.h
+++ include/directui/accessibility.h
@@ -12,33 +12,98 @@ namespace DirectUI
 		
 		void Initialize(Element *);
 		static HRESULT WINAPI Create(Element *, DuiAccessible **);
-
+		static Element * WINAPI GetAccessibleParent(Element *);
 		static HRESULT WINAPI AccNavigate(Element *, long, Element **);
-		virtual unsigned long WINAPI AddRef();
-		virtual HRESULT WINAPI Clone(IEnumVARIANT **);
-		virtual HRESULT WINAPI ContextSensitiveHelp(int);
+	
 		virtual HRESULT Disconnect(void);
 
-		static Element * WINAPI GetAccessibleParent(Element *);
-		virtual HRESULT WINAPI GetIDsOfNames(const IID &, LPOLESTR *,  UINT, LCID, DISPID *);
-		virtual HRESULT WINAPI GetIdentityString(unsigned long, unsigned char **, unsigned long *);
-		virtual HRESULT WINAPI GetTypeInfo(unsigned int, unsigned long, ITypeInfo **);
-		virtual HRESULT WINAPI GetTypeInfoCount(unsigned int *);
-		virtual HRESULT WINAPI GetWindow(HWND *);
+		// IUnknown
+		virtual HRESULT WINAPI QueryInterface(REFIID riid, void **ppvObject);
+		virtual ULONG WINAPI AddRef();
+		virtual ULONG WINAPI Release();
+		
+		// IDispatch
+		virtual HRESULT WINAPI GetTypeInfoCount(UINT *pctinfo);
+		virtual HRESULT WINAPI GetTypeInfo(UINT iTInfo, LCID lcid, ITypeInfo **ppTInfo  );
+		virtual HRESULT WINAPI GetIDsOfNames(REFIID riid, LPOLESTR *rgszNames, UINT cNames, LCID lcid, DISPID *rgDispId);	
+		virtual HRESULT WINAPI Invoke(DISPID dispIdMember, REFIID riid, LCID lcid, WORD wFlags, DISPPARAMS *pDispParams, VARIANT *pVarResult, EXCEPINFO *pExcepInfo, UINT *puArgErr);
+		
+		// IAccIdentity
+		virtual HRESULT WINAPI GetIdentityString(DWORD dwIDChild, BYTE **ppIDString, DWORD *pdwIDStringLen);
+		
+		// IAccessible
+		virtual HRESULT WINAPI get_accParent(IDispatch **);
+		virtual HRESULT WINAPI get_accChildCount(long *);
+		virtual HRESULT WINAPI get_accChild(VARIANT, IDispatch **);
+		virtual HRESULT WINAPI get_accName(VARIANT,  BSTR *);
+		virtual HRESULT WINAPI get_accValue(VARIANT, BSTR *);
+		virtual HRESULT WINAPI get_accDescription(VARIANT, BSTR *);
+		virtual HRESULT WINAPI get_accRole(VARIANT, VARIANT *);
+		virtual HRESULT WINAPI get_accState(VARIANT, VARIANT *);
+		virtual HRESULT WINAPI get_accHelp(VARIANT, BSTR *);
+		virtual HRESULT WINAPI get_accHelpTopic(BSTR *, VARIANT, long *);
+		virtual HRESULT WINAPI get_accKeyboardShortcut(VARIANT, BSTR *);
+		virtual HRESULT WINAPI get_accFocus(VARIANT *);
+		virtual HRESULT WINAPI get_accSelection(VARIANT *);
+		virtual HRESULT WINAPI get_accDefaultAction(VARIANT, BSTR *);
+		
+		virtual HRESULT WINAPI accSelect(long flagsSelect, VARIANT varID);
+		virtual HRESULT WINAPI accLocation(long *pxLeft, long *pyTop, long *pcxWidth, long *pcyHeight, VARIANT varID);
+		virtual HRESULT WINAPI accNavigate(long navDir, VARIANT varStart, VARIANT *pvarEnd);
+		virtual HRESULT WINAPI accHitTest(long xLeft, long yTop, VARIANT *pvarID);
+		virtual HRESULT WINAPI accDoDefaultAction(VARIANT varID);
+		
+		virtual HRESULT WINAPI put_accName(VARIANT varID, BSTR pszValue);
+		virtual HRESULT WINAPI put_accValue(VARIANT varID, BSTR pszValue);		
+		
+		// IEnumVARIANT
+		virtual HRESULT WINAPI Clone(IEnumVARIANT **ppEnum);
+		virtual HRESULT WINAPI Next(unsigned long celt, VARIANT *rgVar, unsigned long *pCeltFetched);
+		virtual HRESULT WINAPI Reset();
+		virtual HRESULT WINAPI Skip(unsigned long celt);
+		
+		// IOleWindow
+		virtual HRESULT WINAPI GetWindow(HWND *phwnd);
+		virtual HRESULT WINAPI ContextSensitiveHelp(BOOL fEnterMode);
 
-		virtual HRESULT WINAPI Invoke(long, const GUID&, unsigned long, unsigned short, DISPPARAMS*, VARIANT*, EXCEPINFO*, unsigned int *);
+		// IServiceProvider
+		virtual HRESULT WINAPI QueryService(REFGUID guidService, REFIID riid, void **ppv);
+		
+
+	protected: 
+		long GetAccName(VARIANT, int, unsigned short **);
+		long GetAccNameFromContent(unsigned short **);
+		long GetDispatchFromElement(Element *, IDispatch **);
+    };
+
+
+	class HWNDHostAccessible : public DuiAccessible
+	{
+	public:
+		HWNDHostAccessible();
+		virtual ~HWNDHostAccessible();
+
+		long Initialize(Element *, IAccessible *);
+		static HRESULT WINAPI Create(Element *, IAccessible *, DuiAccessible **);
+	
+		//IAccIdentity
+		virtual HRESULT WINAPI GetIdentityString(unsigned long, unsigned char **, unsigned long *);
+	
+		virtual HRESULT WINAPI Clone(IEnumVARIANT **);
+		virtual HRESULT WINAPI ContextSensitiveHelp(int);
+		virtual HRESULT Disconnect();
+		virtual HRESULT WINAPI GetWindow(HWND *);
 		virtual HRESULT WINAPI Next(unsigned long, VARIANT *, unsigned long *);
-		virtual HRESULT WINAPI QueryInterface(const GUID&, void **);
-		virtual HRESULT WINAPI QueryService(const GUID&, const GUID&, void **);
-		virtual unsigned long WINAPI Release();
+		virtual HRESULT WINAPI QueryInterface(const IID &, void **);
+		virtual HRESULT WINAPI QueryService(const IID &, const IID &, void **);
 		virtual HRESULT WINAPI Reset();
 		virtual HRESULT WINAPI Skip(unsigned long);
 		virtual HRESULT WINAPI accDoDefaultAction(VARIANT);
-		virtual HRESULT WINAPI accHitTest(long, long, VARIANT*);
+		virtual HRESULT WINAPI accHitTest(long, long, VARIANT *);
 		virtual HRESULT WINAPI accLocation(long *, long *, long *, long *, VARIANT);
 		virtual HRESULT WINAPI accNavigate(long, VARIANT, VARIANT *);
 		virtual HRESULT WINAPI accSelect(long, VARIANT);
-
+		
 		// IAccessible
 		virtual HRESULT WINAPI get_accChild(VARIANT, IDispatch **);
 		virtual HRESULT WINAPI get_accChildCount(long *);
@@ -56,61 +121,6 @@ namespace DirectUI
 		virtual HRESULT WINAPI get_accValue(VARIANT, BSTR *);
 		virtual HRESULT WINAPI put_accName(VARIANT, BSTR);
 		virtual HRESULT WINAPI put_accValue(VARIANT, BSTR);
-	
-	
-	
-	
-	
-	
-	
-	protected: 
-		long GetAccName(VARIANT, int, unsigned short **);
-		long GetAccNameFromContent(unsigned short **);
-		long GetDispatchFromElement(Element *, IDispatch **);
-    };
-
-
-	class HWNDHostAccessible : public DuiAccessible
-	{
-	public:
-		HWNDHostAccessible();
-		virtual ~HWNDHostAccessible();
-
-		long Initialize(Element *, IAccessible *);
-		static HRESULT WINAPI Create(Element *, IAccessible *, DuiAccessible **);
-	
-		virtual HRESULT WINAPI Clone(IEnumVARIANT **);
-		virtual HRESULT WINAPI ContextSensitiveHelp(int);
-		virtual HRESULT Disconnect();
-		virtual HRESULT WINAPI GetIdentityString(unsigned long, unsigned char **, unsigned long *);
-		virtual HRESULT WINAPI GetWindow(HWND *);
-		virtual HRESULT WINAPI Next(unsigned long, VARIANT *, unsigned long *);
-		virtual HRESULT WINAPI QueryInterface(const GUID&, void **);
-		virtual HRESULT WINAPI QueryService(const GUID&, const GUID&, void **);
-		virtual HRESULT WINAPI Reset();
-		virtual HRESULT WINAPI Skip(unsigned long);
-		virtual HRESULT WINAPI accDoDefaultAction(VARIANT);
-		virtual HRESULT WINAPI accHitTest(long, long, VARIANT *);
-		virtual HRESULT WINAPI accLocation(long *, long *, long *, long *, VARIANT);
-		virtual HRESULT WINAPI accNavigate(long, VARIANT, VARIANT *);
-		virtual HRESULT WINAPI accSelect(long, VARIANT);
-
-		virtual HRESULT WINAPI get_accChild(VARIANT, IDispatch **);
-		virtual HRESULT WINAPI get_accChildCount(long *);
-		virtual HRESULT WINAPI get_accDefaultAction(VARIANT, unsigned short **);
-		virtual HRESULT WINAPI get_accDescription(VARIANT, unsigned short **);
-		virtual HRESULT WINAPI get_accFocus(VARIANT *);
-		virtual HRESULT WINAPI get_accHelp(VARIANT, unsigned short **);
-		virtual HRESULT WINAPI get_accHelpTopic(unsigned short **, VARIANT, long *);
-		virtual HRESULT WINAPI get_accKeyboardShortcut(VARIANT, unsigned short **);
-		virtual HRESULT WINAPI get_accName(VARIANT, unsigned short **);
-		virtual HRESULT WINAPI get_accParent(struct IDispatch **);
-		virtual HRESULT WINAPI get_accRole(VARIANT, VARIANT *);
-		virtual HRESULT WINAPI get_accSelection(VARIANT *);
-		virtual HRESULT WINAPI get_accState(VARIANT, VARIANT *);
-		virtual HRESULT WINAPI get_accValue(VARIANT, unsigned short **);
-		virtual HRESULT WINAPI put_accName(VARIANT, unsigned short *);
-		virtual HRESULT WINAPI put_accValue(VARIANT, unsigned short *);
 	};
 
 	class HWNDHostClientAccessible
--- test/coverage/accessibility.cpp
+++ test/coverage/accessibility.cpp
@@ -12,6 +12,10 @@ void accessibility_Accessibility()
 
 void DuiAccessible_coverage()
 {
+	// stuff needed for calls
+	VARIANT v;
+	const IID id;
+
 	// static members
 	DuiAccessible::Create(NULL, NULL);
 	DuiAccessible::AccNavigate(NULL, 0, NULL);
@@ -28,25 +32,90 @@ void DuiAccessible_coverage()
 	k.Disconnect();
 	
 	//
-	//k.GetIDsOfNames(const GUID&, unsigned short **, unsigned int, unsigned long, long *);
+	k.GetIDsOfNames(id, NULL, 0, 0, NULL);
 	k.GetIdentityString(0, NULL,  NULL);
 	k.GetTypeInfo(NULL, NULL, NULL);
 	k.GetTypeInfoCount( NULL );
 	k.GetWindow(NULL);
 	
-	
-	//k.Invoke(long, const GUID&, unsigned long, unsigned short, DISPPARAMS*, VARIANT*, EXCEPINFO*, unsigned int *);
+	//
+	k.Invoke(0, id, 0, 0, NULL, NULL, NULL, NULL);
 	k.Next(0, NULL, NULL);
-	//k.QueryInterface(const GUID&, void **);
-	//k.QueryService(const GUID&, const GUID&, void **);
+	k.QueryInterface(id, NULL);
+	k.QueryService(id, id, NULL);
 	k.Release();
 	k.Reset();
 	k.Skip(0);
-	//k.accDoDefaultAction(VARIANT);
+	k.accDoDefaultAction(v);
 	k.accHitTest(0, 0, NULL);
-	//k.accLocation(NULL, NULL, NULL, NULL, VARIANT);
-	//k.accNavigate(long, VARIANT, VARIANT *);
-	//k.accSelect(long, VARIANT);
+	k.accLocation(NULL, NULL, NULL, NULL, v);
+	k.accNavigate(0, v, NULL);
+	k.accSelect(0, v);
 
+	//
+	k.get_accChild(v, NULL);
+	k.get_accChildCount(NULL);
+	k.get_accDefaultAction(v, NULL);
+	k.get_accDescription(v, NULL);
+	k.get_accFocus(NULL);
+	k.get_accHelp(v, NULL);
+	k.get_accHelpTopic(NULL, v, NULL);
+	k.get_accKeyboardShortcut(v, NULL);
+	k.get_accName(v,  NULL);
+	k.get_accParent(NULL);
+	k.get_accRole(v, NULL);
+	k.get_accSelection(NULL);
+	k.get_accState(v, NULL);
+	k.get_accValue(v, NULL);
+	k.put_accName(v, NULL);
+	k.put_accValue(v, NULL);
+	
 }
 
+void HWNDHostAccessible_coverage() 
+{
+	// stuff needed for calls
+	VARIANT v;
+	const IID id;
+	
+	// static members
+	HWNDHostAccessible::Create(NULL, NULL, NULL);
+
+	HWNDHostAccessible k;
+	k.Initialize(NULL, NULL);
+
+	//
+	k.Clone(NULL);
+	k.ContextSensitiveHelp(0);
+	k.Disconnect();
+	k.GetIdentityString(0, NULL, NULL);
+	k.GetWindow(NULL);
+	k.Next(0, NULL, NULL);
+	k.QueryInterface(id, NULL);
+	k.QueryService(id, id, NULL);
+	k.Reset();
+	k.Skip(0);
+	k.accDoDefaultAction(v);
+	k.accHitTest(0, 0, NULL);
+	k.accLocation(NULL, NULL, NULL, NULL, v);
+	k.accNavigate(0, v, NULL);
+	k.accSelect(0, v);
+
+	//
+	k.get_accChild(v, NULL);
+	k.get_accChildCount(NULL);
+	k.get_accDefaultAction(v, NULL);
+	k.get_accDescription(v, NULL);
+	k.get_accFocus(NULL);
+	k.get_accHelp(v, NULL);
+	k.get_accHelpTopic(NULL, v, NULL);
+	k.get_accKeyboardShortcut(v, NULL);
+	k.get_accName(v, NULL);
+	k.get_accParent(NULL);
+	k.get_accRole(v, NULL);
+	k.get_accSelection(NULL);
+	k.get_accState(v, NULL);
+	k.get_accValue(v, NULL);
+	k.put_accName(v, NULL);
+	k.put_accValue(v, NULL);
+}
